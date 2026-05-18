0ab435d7a1f6d6eabfd59b02ba4486a871b61c5c
腐海 <86948417+RottenSea@users.noreply.github.com>
2026-05-18 04:01:21 -0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,51 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  },
   integrations: [react(), partytown()],
+  fonts: [
+    {
+      name: "Noto Serif SC",
+      cssVariable: "--font-noto-serif-sc",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/Noto_Serif_SC_woff2/NotoSerifSC-Medium.woff2",
+            ],
+            weight: 500,
+            style: "normal",
+          },
+        ],
+      },
+    },
+    {
+      name: "Source Han Sans SC",
+      cssVariable: "--font-source-han-sans-sc",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/SourceHanSansSC_woff2/SourceHanSansSC-Medium.woff2",
+            ],
+            weight: 500,
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -6,8 +6,8 @@ const blog = defineCollection({
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
     title: z.string(),
-    date: z.coerce.date(),
     description: z.string().optional(),
+    date: z.coerce.date(),
     tags: z.array(z.string()).optional(),
   }),
 });
